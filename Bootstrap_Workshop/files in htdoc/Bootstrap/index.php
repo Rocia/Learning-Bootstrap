58ca5585672e750f7b2da82f560337e30dbc6403
ROCIA FERNANDES <rocia.fernandes@gmail.com>
2017-06-24 17:32:40 +0530

    running bootstrp using cdn and table properties

---
+++ Bootstrap_Workshop/files in htdoc/Bootstrap/index.php
@@ -0,0 +1,59 @@
+<html>
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css"><title>
+This is an example of CDN bootstrap and connection with a database</title>
+
+
+<body>
+<?php/*
+//database connection
+$conn = mysqli_connect("localhost","root","","Angular_Bootstrap");
+$sql = "select * from product";
+$result = mysqli_query($conn,$sql);
+if (mysqli_num_rows($result)>0){
+	while ($row = mysqli_fetc_assoc($result)){
+		echo"";
+	}
+}*/
+?>
+<table class="table table-bordered table-striped table-hover" >
+  <tr>
+    <th>Company</th>
+    <th>Contact</th>
+    <th>Country</th>
+  </tr>
+  <tr>
+    <td>Alfreds Futterkiste</td>
+    <td>Maria Anders</td>
+    <td>Germany</td>
+  </tr>
+  <tr>
+    <td>Centro comercial Moctezuma</td>
+    <td>Francisco Chang</td>
+    <td>Mexico</td>
+  </tr>
+  <tr>
+    <td>Ernst Handel</td>
+    <td>Roland Mendel</td>
+    <td>Austria</td>
+  </tr>
+  <tr>
+    <td>Island Trading</td>
+    <td>Helen Bennett</td>
+    <td>UK</td>
+  </tr>
+  <tr>
+    <td>Laughing Bacchus Winecellars</td>
+    <td>Yoshi Tannamuri</td>
+    <td>Canada</td>
+  </tr>
+  <tr>
+    <td>Magazzini Alimentari Riuniti</td>
+    <td>Giovanni Rovelli</td>
+    <td>Italy</td>
+  </tr>
+</table>
+<body>
+</html>
